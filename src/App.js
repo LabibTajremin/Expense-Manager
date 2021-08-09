8eda9cd240e7fccae8d89580cf53d36e0fc5079a
Labib Tajremin <69241061+LabibTajremin@users.noreply.github.com>
2021-08-10 02:57:20 +0600

    2nd update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
+
+const Dummy_Expences = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  
+  const [expenses,setExpenses]=useState(Dummy_Expences);
 
   // *****ALTERNATIVE CODE OF JSX*****
 
@@ -31,7 +34,9 @@ function App() {
   // );
 
   function addExpeseHandler (newexpense){
-    console.log(newexpense);
+    setExpenses(prevExpenses => {
+      return [newexpense,...prevExpenses];
+    });
   }
   return (
     <div>
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -7,33 +7,30 @@ import ExpensesFilter from "./ExpensesFilter";
 
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2020");
+  
   function saveExpensesYearHandler(selectedYear) {
     setFilteredYear(selectedYear);
   }
+  
+  const filteredExpense = props.items.filter(
+    (filter) => filter.date.getFullYear().toString() === filteredYear
+  );
+
   return (
     <div>
       <Card className="expenses">
-        <ExpensesFilter seleced={filteredYear} onChangeExpenseYear={saveExpensesYearHandler} />
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
+        <ExpensesFilter
+          seleced={filteredYear}
+          onChangeExpenseYear={saveExpensesYearHandler}
         />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        />
+        {filteredExpense.map((expenses) => (
+          <ExpenseItem
+            key={expenses.id}
+            title={expenses.title}
+            amount={expenses.amount}
+            date={expenses.date}
+          />
+        ))}
       </Card>
     </div>
   );
